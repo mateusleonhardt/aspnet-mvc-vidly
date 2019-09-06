20fe6e9bba6b6731e04ddae8d82b23d2ff496aaf
Mateus Leonhardt <mleonhardt91@hotmail.com>
2019-09-06 17:41:45 -0300

    Vidly criations

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDBContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDBContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id) {
+            return Content("id=" + id);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Vidly/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; } // My domain models
+        public DbSet<Movie> Movies { get; set; }// My domain models
+    }
+}
